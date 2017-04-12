44fa76e4df68dd2889817099fdc2097c2a3c49af
Daniel Heinrich <dannynullzwo@gmail.com>
2017-04-12 16:29:15 +0200

    remove unnessesary maven central deploy scripts

--- deploy/deploy-to-maven-central.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_79634f27fd4a_key -iv $encrypted_79634f27fd4a_iv -in deploy/codesigning.asc.enc -out deploy/codesigning.asc -d
-    gpg --fast-import deploy/codesigning.asc
-
-    mvn deploy --settings deploy/settings.xml -DperformRelease=true -DskipTests=true
-    exit $?
-fi
